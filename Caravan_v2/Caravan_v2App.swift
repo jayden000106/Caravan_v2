8c6d4dbac4901922f3867c3b80a370494a9775a2
jayden000106 <cars0106@icloud.com>
2022-05-06 03:18:45 +0900

    Initial Commit

---
+++ Caravan_v2/Caravan_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  Caravan_v2App.swift
+//  Caravan_v2
+//
+//  Created by 정지혁 on 2022/05/06.
+//
+
+import SwiftUI
+
+@main
+struct Caravan_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Caravan_v2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Caravan_v2
+//
+//  Created by 정지혁 on 2022/05/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
